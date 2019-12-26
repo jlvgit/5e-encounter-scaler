c395f0030c4ef67d0457b7141fe91cfccf836ccc
jlvgit <jlvel44@gmail.com>
2019-12-26 08:56:34 -0500

    adding player number inputs

--- src/components/encounter.js
+++ src/components/encounter.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 
 //COMPONENTS
 import PartyMembers from './party_members'
+import EncounterXP from './encounter_xp';
 
 class Encounter extends Component {
 
         state = {
-            monsters: this.state.encounter,
-            players: 0,
+            monsters: this.props.encounter,
+            players: 1,
             playerLevel: 1
         }
+
+        setPlayers     = (event) => { this.setState({players:event.target.value}) }
+        setPlayerLevel = (event) => { this.setState({playerLevel:event.target.value}) }
+
 
         render() {
             const encounterCreatures = this.props.encounter.map( (creature) => {
@@ -18,7 +23,7 @@ class Encounter extends Component {
 
             return(
                 <div className='encounter'>
-                    <PartyMembers/>
+                    <PartyMembers setPlayers={this.setPlayers}/>
                     <h5>Encounter</h5>
                     {encounterCreatures}
                 </div>
---
+++ src/components/encounter_xp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EncounterXP = (props) => {
+        return(
+            <div>
+                
+            </div>
+        )
+}
+
+export default EncounterXP;
