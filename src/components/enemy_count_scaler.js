c38a4ec9f5b6612e7b61da2441310589b556beba
jlvgit <jlvel44@gmail.com>
2019-12-29 18:39:36 -0500

    placeholder for new version of component

--- src/components/enemy_count_scaler.js
+++ src/components/enemy_count_scaler.js
@@ -2,43 +2,17 @@ import React from 'react';
 
 const EnemyCountScaler = (props) => {
 
-        const baseXpForFourPlayers = props.getXP(props.monsters, 4)
-        const calculatedXp = props.getXP(props.monsters, props.players)
+        const baseXpForFourPlayers = `${props.getXP(props.monsters, 4)}`
+        const calculatedXp = `${props.getXP(props.monsters, props.players)}`
 
-        const adjustEnemyCount = (base, encounter, playerCount, monsters) => {
-            if (base > encounter){
-                let xpDiffs = []
-                monsters.forEach((creature) => {
-                    let newXp = encounter
-                    let oldCount = creature.count
-                    let newCount = 0
-                    while (base > newXp) {
-                        creature.count ++
-                        newCount++
-                        newXp = props.getXP(monsters, playerCount)
-                    }
-                    creature.count = oldCount
-                    xpDiffs.push({xpDiff:newXp-base, name:creature.name, newCount: newCount})
-                });
-                return xpDiffs
-            } else {
-                return []
-            }
-            
+        const adjustEnemyCount = () => {
+            return "nothing here yet"
         }
-
-        const adjustments = adjustEnemyCount(baseXpForFourPlayers, calculatedXp, props.players, props.monsters)
-        const showAdjustments = adjustments.map((creature) => {
-            return (
-                <div key={creature.name}> Add {creature.newCount}x {creature.name}</div>
-            )
-        })
-
 
         return(
                 <div>
-                    <strong>Adding/Removing enemies</strong>
-                    {showAdjustments}
+                    <strong>Adding/Remove enemies</strong>
+                    {adjustEnemyCount()}
                 </div>
             )
 }
