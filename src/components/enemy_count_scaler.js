62a1f654c77eed896694f348a12d87937402f9c9
jlvgit <jlvel44@gmail.com>
2019-12-30 20:21:08 -0500

    refactoring scaler

--- src/components/enemy_count_scaler.js
+++ src/components/enemy_count_scaler.js
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Range } from 'react-materialize';
 
-const EnemyCountScaler = (props) => {
+import CountSlider from './enemy_count_range_slider'
 
-        const baseXpForFourPlayers = `${props.getXP(props.monsters, 4)}`
-        const calculatedXp = `${props.getXP(props.monsters, props.players)}`
+class EnemyCountScaler extends Component {
 
-        const calculateNewXp = (event) => {
-            console.log(event.target.value)
+        state = {
+            calculatedXp: ''
+        }
+    
+        calculateNewXp = (event, creature) => {
+            let oldCount = creature.count
+            creature.count = event.target.value
+            this.setState({
+                calculatedXp: this.props.getXP(this.props.monsters, this.props.players)
+            })
+            creature.count = oldCount
         }
 
-        const enemies = props.monsters.map((monster) => {
-            return (
-                <div key={monster.name}>            
-                    <label>{monster.name}</label>
-                    <Range  max="30" min="0" name="enemies" defaultValue={monster.count} onChange={calculateNewXp}/>
-                </div>
-            )
-        }) 
+        getEnemySliders = () => {
+            let enemies = this.props.monsters.map((monster) => {
+                return (
+                    <div key={monster.name}>            
+                        <label>{monster.name}</label>
+                        <Range  max="20" min="0" name="enemies" defaultValue={monster.count+1} onChange={(e) => this.calculateNewXp(e, monster)}/>
+                    </div>
+                )
+            }) 
+            return enemies
+        }
 
-        return(
+        render(){
+
+            return(
                 <div>
-                    <strong>Adding/Remove enemies</strong>
-                    {enemies}
+                    <strong>Add/Remove enemies</strong>
+                    <div>XP for {this.props.players} players: {this.state.calculatedXp}</div>
+                    <div>{this.getEnemySliders()}</div>
                 </div>
             )
+        }
 }
 
 export default EnemyCountScaler;
