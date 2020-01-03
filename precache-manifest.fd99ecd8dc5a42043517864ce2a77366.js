9cef6e19dd8aca92089724e0a7f9c9d34bb12cb7
jlvgit <jlvel44@gmail.com>
2020-01-02 19:07:50 -0500

    Updates

---
+++ precache-manifest.fd99ecd8dc5a42043517864ce2a77366.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d400f66363b0cc885b96498f14a0e20",
+    "url": "/5e-encounter-scaler/index.html"
+  },
+  {
+    "revision": "2c6bc3e1b7f11b906311",
+    "url": "/5e-encounter-scaler/static/css/main.22349efc.chunk.css"
+  },
+  {
+    "revision": "9b4ca7b2e5f54120e22a",
+    "url": "/5e-encounter-scaler/static/js/2.933c6a70.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/5e-encounter-scaler/static/js/2.933c6a70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c6bc3e1b7f11b906311",
+    "url": "/5e-encounter-scaler/static/js/main.c2f00dc1.chunk.js"
+  },
+  {
+    "revision": "03194368ebed7180fdd0",
+    "url": "/5e-encounter-scaler/static/js/runtime-main.36f59f58.js"
+  }
+]);
